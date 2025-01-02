0fe228406f24994d39bf2706239bbf025f1c856e
Samado Xen0w <benazouz.souheilabdessamed@gmail.com>
2025-01-03 00:22:14 +0100

    3em

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use("/", itemRouter);
 
-app.use("/categories", categoryRouter);
-app.use("/items", itemRouter);
+app.use("/", categoryRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
